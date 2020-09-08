14f945c1c6bb0dcdb54ec11adbf2cea4c93bfa5f
KNRiya <khayrun.nahar@northsouth.edu>
2020-09-09 02:27:36 +0600

    2nd commit

---
+++ app/src/main/java/com/example/mycard/RyaRiyagsfxvousybsyud.java
@@ -0,0 +1,14 @@
+package com.example.mycard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RyaRiyagsfxvousybsyud extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rya_riyagsfxvousybsyud);
+    }
+}
