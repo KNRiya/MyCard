48b961093e12d5747f3d084ac7344d81635a299d
KNRiya <khayrun.nahar@northsouth.edu>
2020-09-25 02:17:28 +0600

    location

--- app/src/main/java/com/example/mycard/MainActivity.java
+++ app/src/main/java/com/example/mycard/MainActivity.java
@@ -8,14 +8,23 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-private CardView hospitalCardView;
+private CardView hospitalCardView , locationCardview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         hospitalCardView= findViewById(R.id.hospitalCardviewId1);
+        locationCardview=findViewById(R.id.hospitalCardviewId2);
 
         hospitalCardView.setOnClickListener(this);
+
+        locationCardview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,Location.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
